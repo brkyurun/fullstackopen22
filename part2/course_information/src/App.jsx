a7ab9066a0a4679c2fafc9744f5ed0c4c33c00aa
Berkay Urun <berkayurun@outlook.com>
2022-11-30 15:31:27 +0300

    complete challenge 2.5

--- part2/course_information/src/App.jsx
+++ part2/course_information/src/App.jsx
@@ -1,36 +1,4 @@
-const Header = ({ course }) => <h2>{course.name}</h2>;
-
-const Total = ({ course }) => {
-  const numbers = course.parts.map((part) => part.exercises);
-  const total = numbers.reduce((prev, curr) => {
-    return prev + curr;
-  });
-  return <p>total of {total} exercises</p>;
-};
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-);
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total course={course} />
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
