7d9fd3a9fec125a83347f5bbeb8b1d3b1e18dc60
Berkay Urun <berkayurun@outlook.com>
2022-12-09 18:14:10 +0300

    update api link

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api/persons/": {
+        target: "https://phonebook-practice-backend.onrender.com/api/persons",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/persons/, ""),
+      },
+    },
+  },
+});
