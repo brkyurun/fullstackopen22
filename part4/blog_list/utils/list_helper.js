c48c25d9792394d3ca37dc386ca1834b5d6070dd
Berkay Urun <berkayurun@outlook.com>
2022-12-30 13:30:51 +0300

    complete challenge 4.5

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.map((blog) => blog.likes).reduce((prev, curr) => prev + curr, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const mostLikes = Math.max(...blogs.map((blog) => blog.likes));
+  const result = blogs.find((blog) => blog.likes === mostLikes);
+  return result;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
